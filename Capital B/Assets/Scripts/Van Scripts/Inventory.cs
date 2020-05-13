99d22b47b26f4a89571af66d1a3d253e454ad1ed
Van Phan <infernosdaavenger@gmail.com>
2020-05-13 17:14:00 -0400

    Adding item and inventory
    Item and inventory added

---
+++ Capital B/Assets/Scripts/Van Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+    public static Inventory instance;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("More than once inventory found.");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public List<Item> items = new List<Item>();
+
+    public void Add(Item item)
+    {
+        if (!item.isDefaultItem)
+        {
+            items.Add(item);
+        }
+    }
+
+    public void Remove (Item item)
+    {
+        items.Remove(item);
+    }
+}
--- Capital B/Assets/Scripts/Van Scripts/ItemPickup.cs
+++ Capital B/Assets/Scripts/Van Scripts/ItemPickup.cs
@@ -19,6 +19,7 @@ public class ItemPickup : Interactable
     void PickUp()
     {
         Debug.Log("Picked up  " + transform.name);
+        Inventory.instance.Add(Item); 
         Destroy(gameObject);
     }
 
