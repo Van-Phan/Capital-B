9b642d2e38f4f06adf97787dab599d2c6ebb3fcb
jdh5563 <56982973+jdh5563@users.noreply.github.com>
2020-05-13 17:55:14 -0400

    Changed Enemy Targeting

--- Capital B/Assets/Scripts/John Scripts/EnemyManger.cs
+++ Capital B/Assets/Scripts/John Scripts/EnemyManger.cs
@@ -12,7 +12,10 @@ public class EnemyManger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        enemy.GetComponent<EnemyMovement>().target = target;
+        if (target != null)
+        {
+            enemy.GetComponent<EnemyMovement>().target = target;
+        }
     }
 
     // Update is called once per frame
